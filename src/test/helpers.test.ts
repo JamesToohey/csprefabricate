3d56f2671a7d5d3cd50b3127a3d017f0fb7c076e
James Toohey <james@toohey.io>
2025-05-25 08:43:26 +1000

    Add eslint (#12)

--- src/test/helpers.test.ts
+++ src/test/helpers.test.ts
@@ -3,24 +3,24 @@ import assert from "node:assert";
 import {formatRule, isValidDirective} from "../helpers";
 import {Directive} from "../types";
 
-describe("Helpers tests", () => {
-    describe("isValidDirective", () => {
-        it("Returns true if directive is valid", () => {
+void describe("Helpers tests", () => {
+    void describe("isValidDirective", () => {
+        void it("Returns true if directive is valid", () => {
             assert.strictEqual(isValidDirective(Directive.BASE_URI), true);
             assert.strictEqual(isValidDirective("default-src"), true);
         });
 
-        it("Returns false if directive is invalid", () => {
+        void it("Returns false if directive is invalid", () => {
             assert.strictEqual(isValidDirective("some-src"), false);
         });
     });
 
-    describe("formatRule", () => {
-        it("Formats special rules with single quotes", () => {
+    void describe("formatRule", () => {
+        void it("Formats special rules with single quotes", () => {
             assert.strictEqual(formatRule("self"), `'self'`);
         });
 
-        it("Returns non-special rules", () => {
+        void it("Returns non-special rules", () => {
             assert.strictEqual(formatRule("google.com"), `google.com`);
         });
     });
